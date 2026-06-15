c205294e6f13d7e3bf6d3e0dcc4349c56f55446b
officenotfound <404officenotfound@gmail.com>
2026-06-15 19:28:41 -0400

    Nudge PR right to 0.28em

--- app/page.tsx
+++ app/page.tsx
@@ -127,7 +127,7 @@ export default function Home() {
                 fontFamily:MONO,
                 fontSize:"0.18em",
                 marginTop:"0.1em",
-                marginLeft:"0",
+                marginLeft:"0.28em",
                 letterSpacing:"0.25em",
                 color:t.muted,
                 textTransform:"uppercase",
