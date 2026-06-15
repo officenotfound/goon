a90571c3231c50570ceec7703a8f151dfd26ba16
officenotfound <404officenotfound@gmail.com>
2026-06-15 19:50:43 -0400

    Fix PR positioning using vw units instead of em

--- app/page.tsx
+++ app/page.tsx
@@ -127,7 +127,7 @@ export default function Home() {
                 fontFamily:MONO,
                 fontSize:"0.18em",
                 marginTop:"0.1em",
-                marginLeft:"0.2em",
+                marginLeft:"3.5vw",
                 letterSpacing:"0.25em",
                 color:t.muted,
                 textTransform:"uppercase",
