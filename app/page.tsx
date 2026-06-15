d77c6b4b9ff13c4600b6b663d2faffcd11b1c465
officenotfound <404officenotfound@gmail.com>
2026-06-15 19:36:16 -0400

    PR centered under middle of G (0.32em)

--- app/page.tsx
+++ app/page.tsx
@@ -127,7 +127,7 @@ export default function Home() {
                 fontFamily:MONO,
                 fontSize:"0.18em",
                 marginTop:"0.1em",
-                marginLeft:"0.56em",
+                marginLeft:"0.32em",
                 letterSpacing:"0.25em",
                 color:t.muted,
                 textTransform:"uppercase",
